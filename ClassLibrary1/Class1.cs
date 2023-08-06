68b055254e758256807cd286a4dcd0c0070dca6f
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-08-06 22:52:46 +0300

    Set Methods

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -16,6 +16,72 @@
         dateOfPurchase = System.DateTime.Now.ToShortDateString();
     }
 
+    //Set method for productID
+    public void SetProductID(int value)
+    {
+        productID = value;
+    }
+
+    //Get Method for productId
+    public int GetProductID()
+    {
+        return productID;
+    }
+
+    //Set method for productName
+    public void SetProductName(string value)
+    {
+        productName = value;
+    }
+
+    //Get Method for productName
+    public string GetProductName()
+    {
+        return productName;
+    }
+
+    //Set method for Cost
+    public void SetCost(double value)
+    {
+        cost = value;
+    }
+
+    //Get Method for Cost
+    public double GetCost()
+    {
+        return cost;
+    }
+
+    //Set method for tax
+    public void SetTax(double value)
+    {
+        tax = value;
+    }
+
+    //Get Method for Tax
+    public double GetTax()
+    {
+        return tax;
+    }
+
+    //Set method for quantityInStock
+    public void SetQuantityInStock(int value)
+    {
+        quantityInStock = value;
+    }
+
+    //Get Method for quantityInStock
+    public int GetQuantityInStock()
+    {
+        return quantityInStock;
+    }
+
+    //Get Method for dateOfPurchase
+    public string GetDateOfPurchase()
+    {
+        return dateOfPurchase;
+    }
+
     //method
     /*cost <= 20000  then  tax = 10%
     cost > 20000 then  tax = 12.5%*/
--- MethodsExample/Program.cs
+++ MethodsExample/Program.cs
@@ -16,19 +16,19 @@
         product3 = new Product();
         Product.TotalNoProducts++; //3
 
-        //initalize fields
-        product1.productID = 1001;
-        product1.productName = "Mobile";
-        product1.cost = 20000;
-        product1.quantityInStock = 1200;
-        product2.productID = 1003;
-        product2.productName = "Laptop";
-        product2.cost = 45000;
-        product2.quantityInStock = 3400;
-        product3.productID = 1003;
-        product3.productName = "Speakers";
-        product3.cost = 36000;
-        product3.quantityInStock = 800;
+        //initIalize fields
+        product1.SetProductID(1001);
+        product1.SetProductName("Mobile");
+        product1.SetCost(20000;
+        product1.SetQuantityInStock(1200);
+        product2.SetProductID(1003);
+        product2.SetProductName("Laptop");
+        product2.SetCost(45000);
+        product2.SetQuantityInStock(3400);
+        product3.SetProductID(1003);
+        product3.SetProductName("Speakers");
+        product3.SetCost(36000);
+        product3.SetQuantityInStock(800);
 
         //call methods
         product1.CalculateTax();
